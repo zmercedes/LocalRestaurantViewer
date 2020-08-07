c9a3a0e91ae2e7c614b29887b485cbb03905ce85
zmercedes <zmercedes89@gmail.com>
2020-08-07 18:56:53 -0400

    feat: AppDelegate: add LocationManagerDelegate extension and DataProvider

--- LocalRestaurantViewer/AppDelegate.swift
+++ LocalRestaurantViewer/AppDelegate.swift
@@ -13,13 +13,27 @@ import CoreLocation
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let locationManager = CLLocationManager()
+    let lManager = CLLocationManager()
+    var data = DataProvider()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        locationManager.requestAlwaysAuthorization()
+        lManager.requestAlwaysAuthorization()
+        lManager.delegate = self
+        lManager.requestLocation()
         
         return true
     }
 }
 
+extension AppDelegate: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("get location failed")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        data.fetchData(lat: location.coordinate.latitude, long: location.coordinate.longitude)
+    }
+}
