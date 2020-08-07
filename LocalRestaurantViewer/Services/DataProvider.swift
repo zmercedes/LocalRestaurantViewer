2d014d4a193f54109701319601c2368a5ae0d47e
zmercedes <zmercedes89@gmail.com>
2020-08-07 18:47:59 -0400

    feat: DataProvider: fill out fetchData and add decodeJson(data:)

--- LocalRestaurantViewer/Services/DataProvider.swift
+++ LocalRestaurantViewer/Services/DataProvider.swift
@@ -11,23 +11,61 @@ import CoreLocation
 
 class DataProvider {
     
-    let locationManager: CLLocationManager
+    init() {}
     
-    init(location: CLLocationManager) {
-        locationManager = location
-        fetchData()
-    }
-    
-    func fetchData() {
-        let url = URL(string: "https://api.yelp.com/v3/businesses/search")!
-        var request = URLRequest(url: url)
+    func fetchData(lat: Double, long: Double) {
+        var components = URLComponents(string: "https://api.yelp.com/v3/businesses/search")!
+        let queryItems = [
+            "latitude":"\(lat)",
+            "longitude":"\(long)",
+            "radius": "800" // 1 mile
+        ]
+        components.queryItems = queryItems.map { (key, value) in
+            URLQueryItem(name: key, value: value)
+        }
+        var request = URLRequest(url: components.url!)
         request.httpMethod = "GET"
         
-        request.setValue("", forHTTPHeaderField: "Authorization")
+        request.setValue(Constants.APIkey.yelp, forHTTPHeaderField: "Authorization")
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("client error: \(error.localizedDescription)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("server error: \(response)")
+                return
+            }
+            if let mime = httpResponse.mimeType, mime == "application/json", let data = data {
+                self.decodeJson(data: data)
+                
+            }
+        }
+        task.resume()
+    }
+    
+    func decodeJson(data: Data){
+        var json: Any?
+        do {
+            json = try JSONSerialization.jsonObject(with: data, options: [])
+        } catch {
+            print("Cannot decode")
+            return
         }
         
+        var newRestaurants: [Restaurant] = []
+        
+        let body = json as! [String: Any]
+        let businesses = body["businesses"] as! [[String: Any]]
+        for business in businesses {
+            let name = business["name"] as! String
+            let rating = business["rating"] as! Float
+            let image = business["image_url"] as! String
+            let distance = business["distance"] as! NSNumber
+            newRestaurants.append(Restaurant(n: name, r: rating, i: image, d: distance.floatValue))
+        }
+        newRestaurants.sort { $0.distance < $1.distance }
+        print("\(newRestaurants.count) added.")
     }
 }
