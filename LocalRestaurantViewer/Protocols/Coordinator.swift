00714883501bb136420b6cf458258d4ed2787d83
zmercedes <zmercedes89@gmail.com>
2020-08-07 19:10:04 -0400

    feat: add Coordinator protocol

---
+++ LocalRestaurantViewer/Protocols/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  LocalRestaurantViewer
+//
+//  Created by Zoilo Mercedes on 8/7/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator: class {
+    func start()
+    func stop()
+}
