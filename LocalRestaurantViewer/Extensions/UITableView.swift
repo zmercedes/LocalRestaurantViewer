3433a84a9edc62660ca2fa1ddc6ef699f2725d18
zmercedes <zmercedes89@gmail.com>
2020-08-07 22:15:49 -0400

    feat: add UITableView extensions
    This commit adds extensions to UITableView which make registering/dequeuing cells much easier.

---
+++ LocalRestaurantViewer/Extensions/UITableView.swift
@@ -0,0 +1,24 @@
+//
+//  UITableView.swift
+//  LocalRestaurantViewer
+//
+//  Created by Zoilo Mercedes on 8/7/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func register<T: UITableViewCell>(_: T.Type) {
+        let nib = UINib(nibName: T.reuseIdentifier, bundle: nil)
+        self.register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        return cell
+    }
+}
