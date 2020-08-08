28d11082dc7d2a07d39c5d56a313b253ceddfc10
zmercedes <zmercedes89@gmail.com>
2020-08-07 22:13:20 -0400

    feat: add ReusableView protocol
    This commit adds the reusable view protocol which allows a reusable view to automatically have a string that it can use to describe itself.

---
+++ LocalRestaurantViewer/Protocols/ReusableView.swift
@@ -0,0 +1,23 @@
+//
+//  ReusableView.swift
+//  LocalRestaurantViewer
+//
+//  Created by Zoilo Mercedes on 8/7/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+    
+}
+
+extension UITableViewCell: ReusableView { }
