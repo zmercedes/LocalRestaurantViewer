41fd52d1402b8be58924b24dc3a8ade4b6ae0fcb
zmercedes <zmercedes89@gmail.com>
2020-08-08 00:32:04 -0400

    feat: AppCoordinator: add Dependencies propagation
    This commit adds dependencies to AppCoordinator, stores them, and passes them into RestaurantCardViewController.

--- LocalRestaurantViewer/Flows/AppCoordinator.swift
+++ LocalRestaurantViewer/Flows/AppCoordinator.swift
@@ -12,12 +12,13 @@ class AppCoordinator: Coordinator {
     
     private let window: UIWindow
     private let rootViewController: UIViewController
+    private let dependencies: Dependencies
     
-    init(window: UIWindow) {
-        self.rootViewController = RestaurantCardViewController()
+    init(window: UIWindow, dependencies: Dependencies) {
         self.window = window
+        self.dependencies = dependencies
+        self.rootViewController = RestaurantCardViewController(dependencies: dependencies)
         self.window.rootViewController = rootViewController
-//        self.window.backgroundColor = .purple
         self.window.makeKeyAndVisible()
     }
     
